d32f01e21b35cefbaeac0c56c915a5fc951a1704
ganam <7ganam@gmail.com>
2023-04-21 14:12:01 +0200

    added projects page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import ProjectsView from "@/views/projects";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+export default function Projects() {
+  return <ProjectsView />;
 }
